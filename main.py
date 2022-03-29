efc0ccfdd1296185851c4d96d6d846d4a89c4054
Matouš Zatloukal <matouszatloukal@seznam.cz>
2022-03-29 23:08:25 +0200

    konecna verze

--- main.py
+++ main.py
@@ -17,14 +17,6 @@ class Application(tk.Tk):
         self.lbl.pack()
         self.btnVypsat = tk.Button(self, text="Načíst ze souboru", command=self.zeSouboru)
         self.btnVypsat.pack()
-        self.lbl1 = tk.Label(self, text="Počáteční čas")
-        self.lbl1.pack()
-        self.entryStart  = tk.Entry(self)
-        self.entryStart.pack()
-        self.lbl2 = tk.Label(self, text="koncový čas")
-        self.lbl2.pack()
-        self.entryKonec  = tk.Entry(self)
-        self.entryKonec.pack()
         self.lbl3 = tk.Label(self, text="Frekvence")
         self.lbl3.pack()
         self.entryF  = tk.Entry(self)
@@ -37,28 +29,21 @@ class Application(tk.Tk):
         self.btnGraf.pack()
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
-        
-
-    def about(self):
-        window = About(self)
-        window.grab_set()
 
     def quit(self, event=None):
         super().quit()
     
     def graf(self):
-        self.f = self.entryF.get()
-        self.a = self.entryA.get()
-        self.k = self.entryKonec.get()
-        self.s = self.entryStart.get()
-        print(self.f)
-        print(self.a)
-        print(self.s)
-        print(self.k)
+        self.f = int(self.entryF.get())
+        self.a = int(self.entryA.get())
 
-        t=linspace(0,50e-3,300)
- 
+        try:
+            t = pl.linspace(1, 10 / self.f, self.f * 10000)
+            x = self.a * (pl.cos(2 * pi * self.f * t))
+        except ZeroDivisionError:
+            print('takhle by to neslo klukoune, zadej tam tu frekvenci')
 
+        pl.plot(t, x)
         pl.title("výkon")
         pl.xlabel("t[s]")
         pl.ylabel("u[V],i[A], p[W]")
